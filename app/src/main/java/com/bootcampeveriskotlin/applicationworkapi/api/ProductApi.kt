d4d06cbad36bb85ab2570cad6cf1de3c4e5a74b1
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2021-03-03 15:40:40 -0300

    Created retrofit class

--- app/src/main/java/com/bootcampeveriskotlin/applicationworkapi/api/ProductApi.kt
+++ app/src/main/java/com/bootcampeveriskotlin/applicationworkapi/api/ProductApi.kt
@@ -1,4 +1,10 @@
 package com.bootcampeveriskotlin.applicationworkapi.api
 
+import com.bootcampeveriskotlin.applicationworkapi.model.Product
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ProductApi {
+    @GET("getdata.php")
+    fun getProductAPI(): Call<List<Product>>
 }
--- app/src/main/java/com/bootcampeveriskotlin/applicationworkapi/model/Product.kt
+++ app/src/main/java/com/bootcampeveriskotlin/applicationworkapi/model/Product.kt
@@ -1,4 +1,18 @@
 package com.bootcampeveriskotlin.applicationworkapi.model
 
-class Product {
-}
+import com.google.gson.annotations.SerializedName
+
+data class Product (
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("prname")
+    val prName: String,
+    @SerializedName("primage")
+    val prImage: String,
+    @SerializedName("prprice")
+    val prPrice: String,
+)
+
+
+
+
